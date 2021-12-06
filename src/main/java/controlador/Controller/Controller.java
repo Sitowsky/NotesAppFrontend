11771e26daabb54485b3a961821d38c50bb6fad9
Carlos Fabricio Alfaro Alonso <48113883+Sitowsky@users.noreply.github.com>
2021-12-06 12:30:35 -0600

    Clases

---
+++ src/main/java/controlador/Controller/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.Controller;
+
+/**
+ *
+ * @author fabri
+ */
+public class Controller {
+    
+}
